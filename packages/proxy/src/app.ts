9533cb8743960020cbbffdde2267f55b5ea050d6
hieudoanm <hieumdoan@gmail.com>
2022-07-17 07:24:58 +0700

    Update Docker Image

--- packages/proxy/src/app.ts
+++ packages/proxy/src/app.ts
@@ -2,6 +2,7 @@ import { errorHandler, notFoundHandler } from '@apis-services/shared';
 import express, { Request, Response, urlencoded, json } from 'express';
 import swaggerUi from 'swagger-ui-express';
 import { RegisterRoutes } from './routes';
+import swaggerJSON from './swagger.json';
 
 const app = express();
 app.use(json());
@@ -9,8 +10,9 @@ app.use(urlencoded({ extended: true }));
 
 RegisterRoutes(app);
 
-app.get('/api/docs', swaggerUi.serve, async (_req: Request, res: Response) => {
-  return res.send(swaggerUi.generateHTML(await import('./swagger.json')));
+app.use(swaggerUi.serve);
+app.get('/api/docs', (_request: Request, response: Response) => {
+  return response.send(swaggerUi.generateHTML(swaggerJSON));
 });
 
 app.use(errorHandler);
